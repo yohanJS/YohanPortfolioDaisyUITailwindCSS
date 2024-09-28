1795a1065a2a3bfc370bb74f2ab4eaad3a2f2fed
Yohan Garcia <yoanvaldes03@gmail.com>
2024-09-28 15:11:11 -0400

    Initial commit. NavBar, HeroSection added

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="hero bg-base-200 min-h-screen">
+  <div class="hero-content flex-col lg:flex-row">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+      class="max-w-sm rounded-lg shadow-2xl" />
+    <div>
+      <h1 class="text-3xl font-bold">Software Engineer</h1>
+      <p class="py-6">
+        I built secure and fast web applications at the enterprise level.
+      </p>
+      <button class="btn btn-primary">View work</button>
+    </div>
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
